54c2d37d2f7dae0a404a9f5c19853058eb9d10d1
ofabriciofarias <63527776+ofabriciofarias@users.noreply.github.com>
2022-01-26 21:45:19 -0300

    Update exemplo14.c

--- exemplo14.c
+++ exemplo14.c
@@ -1,7 +1,7 @@
 /*
-ExercÌcio
-Construir um algoritmo que calcule a mÈdia aritmÈtica de v·rios valores inteiros
-positivos, lidos externamente. O final da leitura acontecer· quando for lido um valor
+Exerc√≠cio
+Construir um algoritmo que calcule a m√©dia aritm√©tica de v√°rios valores inteiros
+positivos, lidos externamente. O final da leitura acontecer√° quando for lido um valor
 negativo.
 
 */
@@ -10,7 +10,7 @@ negativo.
 
 int main(){
 
-    int valor, somaValor = 0;
+    int valor=0, somaValor = 0;
     int contador = 0;
     float mediaValor;
 
